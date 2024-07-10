ce44850b318fb1508dbcee7e42f3f551827d5ba8
StevenMax <stevenmaxtaruna@gmail.com>
2024-07-10 11:27:20 +0700

    menambahkan komponen kotak1&2

---
+++ frontend/src/components/kotak1.jsx
@@ -0,0 +1,7 @@
+function Kotak1() {
+   return(
+    <h1>kotak 1 dari komponen</h1>
+   );
+};
+
+export default Kotak1;
---
+++ frontend/src/components/kotak2.jsx
@@ -0,0 +1,9 @@
+function Kotak2(){
+   return(
+    <>
+    <h1>kotak2 dari komponen</h1>
+    </>
+   );
+};
+
+export default Kotak2;
